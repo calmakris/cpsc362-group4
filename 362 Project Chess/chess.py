8522746eae85a87db0de9b05dfc7085b3e248426
leederrick1 <derricklee@csu.fullerton.edu>
2021-09-22 15:29:12 -0700

    added points counter function

--- 362 Project Chess/chess.py
+++ 362 Project Chess/chess.py
@@ -3,7 +3,8 @@
 class Chess():
 
     def __init__(self):
-
+        black_points = 0
+        white_points = 0
         self.turn = 1
         self.player = 1
         # self.board_change = false could be used to determine if change occured? 
@@ -20,4 +21,12 @@ class Chess():
             [ 1,  1,  1,  1,  1,  1,  1,  1],
             [ 3,  2,  4,  6,  5,  4,  2,  3]
         ]
+    #takes in the piece remove piece to calculate the score and the player to add the points to    
+    def point_counter(self, piece, cur_score):
+        piece_num = piece
+        point_dict = {1:1, 2:3, 4:3, 3:5, 5:9}
+        if piece_num > 10:
+            piece_num = piece_num - 10
+        cur_score = cur_score + point_dict[piece_num]
+        return cur_score
     

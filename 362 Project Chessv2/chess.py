5f7b0efa9038fd69f3d2e3e1688ec123e096f4e6
leederrick1 <derricklee@csu.fullerton.edu>
2021-09-22 15:32:41 -0700

    added points counter function

--- 362 Project Chessv2/chess.py
+++ 362 Project Chessv2/chess.py
@@ -20,4 +20,11 @@ class Chess():
             [ 1,  1,  1,  1,  1,  1,  1,  1],
             [ 3,  2,  4,  6,  5,  4,  2,  3]
         ]
-    
+    #takes in the piece remove piece to calculate the score and the player to add the points to    
+    def point_counter(self, piece, cur_score):
+        piece_num = piece
+        point_dict = {1:1, 2:3, 4:3, 3:5, 5:9}
+        if piece_num > 10:
+            piece_num = piece_num - 10
+        cur_score = cur_score + point_dict[piece_num]
+        return cur_score
